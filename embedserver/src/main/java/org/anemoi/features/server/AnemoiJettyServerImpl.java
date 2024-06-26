7649eb58342ad924ddbaa984e1c33d9c5892abdb
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2024-06-26 15:48:52 +0300

    feat: updating the AnemoiJettyServerImpl to be able to handle jsp

--- embedserver/src/main/java/org/anemoi/features/server/AnemoiJettyServerImpl.java
+++ embedserver/src/main/java/org/anemoi/features/server/AnemoiJettyServerImpl.java
@@ -1,16 +1,29 @@
 package org.anemoi.features.server;
 
 import org.anemoi.features.server.defaultcontroller.DefaultServlet;
+import org.apache.tomcat.InstanceManager;
+import org.apache.tomcat.SimpleInstanceManager;
+import org.eclipse.jetty.jsp.JettyJspServlet;
 import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.io.IOException;
 
 public final class AnemoiJettyServerImpl {
     private Server server;
     private int port;
+    static final String WEB_ROOT_INDEX = "/web/";
     
     
     private final  int maxThreads = 200;
@@ -30,10 +43,28 @@ public final class AnemoiJettyServerImpl {
         connector.setPort(this.port);
         server.setConnectors(new Connector[]{connector});
 
-        ServletHandler servletHandler = new ServletHandler();
-        server.setHandler(servletHandler);
 
-        servletHandler.addServletWithMapping(DefaultServlet.class, "/status");
+        // Create URI for Servlet Context
+        URI  baseURI = getWebRootResourceURI();
+
+
+        // Create Servlet Context
+
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletContextHandler.setContextPath("/");
+        servletContextHandler.setResourceBase(baseURI.toASCIIString());
+
+        enableEmbeddedJspSupport(servletContextHandler);
+
+        
+
+        ServletHolder holderDefault = new ServletHolder("default", DefaultServlet.class);
+        holderDefault.setInitParameter("resourceBase", baseURI.toASCIIString());
+        holderDefault.setInitParameter("dirAllowed", "true");
+        servletContextHandler.addServlet(holderDefault, "/");
+        
+
+        server.setHandler(servletContextHandler);
 
 
         server.start();
@@ -45,6 +76,53 @@ public final class AnemoiJettyServerImpl {
         server.stop();
     }
 
+
+    private URI getWebRootResourceURI() throws FileNotFoundException, URISyntaxException{
+        URL indexURI = this.getClass().getResource(WEB_ROOT_INDEX);
+        if(indexURI == null){
+            throw new FileNotFoundException(" web directory not found in the resources , please create the directory!");
+        }
+        return indexURI.toURI();
+    }
+
+    private void enableEmbeddedJspSupport(ServletContextHandler servletContextHandler) throws IOException{
+            // Establish Scratch directory for the servlet context (used by JSP compilation)
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(), "embedded-jetty-jsp");
+
+        if (!scratchDir.exists())
+        {
+            if (!scratchDir.mkdirs())
+            {
+                throw new IOException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
+        servletContextHandler.setAttribute("jakarta.servlet.context.tempdir", scratchDir);
+
+        // Set Classloader of Context to be sane (needed for JSTL)
+        // JSP requires a non-System classloader, this simply wraps the
+        // embedded System classloader in a way that makes it suitable
+        // for JSP to use
+        ClassLoader jspClassLoader = new URLClassLoader(new URL[0], this.getClass().getClassLoader());
+        servletContextHandler.setClassLoader(jspClassLoader);
+
+        // Manually call JettyJasperInitializer on context startup
+        servletContextHandler.addBean(new EmbeddedJspStarter(servletContextHandler));
+
+        // Create / Register JSP Servlet (must be named "jsp" per spec)
+        ServletHolder holderJsp = new ServletHolder("jsp", JettyJspServlet.class);
+        holderJsp.setInitOrder(0);
+        holderJsp.setInitParameter("scratchdir", scratchDir.toString());
+        holderJsp.setInitParameter("logVerbosityLevel", "DEBUG");
+        holderJsp.setInitParameter("fork", "false");
+        holderJsp.setInitParameter("xpoweredBy", "false");
+        holderJsp.setInitParameter("compilerTargetVM", "1.8");
+        holderJsp.setInitParameter("compilerSourceVM", "1.8");
+        holderJsp.setInitParameter("keepgenerated", "true");
+        servletContextHandler.addServlet(holderJsp, "*.jsp");
+
+        servletContextHandler.setAttribute(InstanceManager.class.getName(), new SimpleInstanceManager());
+    }
 
     
 }
